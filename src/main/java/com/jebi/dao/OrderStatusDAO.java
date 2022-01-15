c2bd481f0dc1134a6f41ae8e77cce27ee9d7800c
zero2-in <wowin21@gmail.com>
2022-01-16 06:05:10 +0900

    안정화

--- src/main/java/com/jebi/dao/OrderStatusDAO.java
+++ src/main/java/com/jebi/dao/OrderStatusDAO.java
@@ -1,5 +1,8 @@
 package com.jebi.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import com.jebi.common.CommonUtil;
 import com.jebi.dto.StatusListDTO;
 
@@ -7,8 +10,24 @@ public class OrderStatusDAO {
     CommonUtil util = new CommonUtil();
 
     public ArrayList<StatusListDTO> getStatusListCategory() {
+        ArrayList<StatusListDTO> list = new ArrayList<>();
+        String debugMethod = new Object(){}.getClass().getEnclosingMethod().getName();
 
+        String query = "";
+
+        util.runQuery(query, debugMethod, 0);
+
+        try {
+            while(util.getRs().next()) {
+
+            }
+        } catch(SQLException e) {
+            util.viewErr(debugMethod);
+        } finally {
+            util.closeDB();
+        }
+
+        return list;
     }
-
-    public int
+    
 }
